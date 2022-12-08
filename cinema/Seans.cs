34c4d34d0815affed3483eeaf81823653f5d0d06
opilane <opilane@HP-CZC6297BB4>
2022-12-08 15:42:53 +0200

    good

--- cinema/Seans.cs
+++ cinema/Seans.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace cinema
@@ -18,12 +10,31 @@ namespace cinema
         //SqlConnection connenction = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\lasto\\source\\repos\\cinema\\cinema\\DB\\cinemaDB.mdf;Integrated Security=True");
         public Seans()
         {
+            SeansFrame seansFrame = new SeansFrame(1);
         }
-        public class SeansFrame 
+        public class SeansFrame : Label
         {
-            public SeansFrame(int seansid) 
+            int filmID;
+            public SeansFrame(int seansid)
             {
-                co
+                SqlConnection connenction = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\opilane.TTHK\\source\\repos\\Lastovski_TARpv21\\cinema\\cinema\\DB\\cinemaDB.mdf;Integrated Security=True");
+                //SqlConnection connenction = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\lasto\\source\\repos\\cinema\\cinema\\DB\\cinemaDB.mdf;Integrated Security=True");
+                SqlCommand cmd;
+                SqlDataReader reader;
+                cmd = new SqlCommand("SELECT time, filmID FROM seanss WHERE Id =" + seansid, connenction);
+                connenction.Open();
+                reader = cmd.ExecuteReader();
+                string time;
+                while (reader.Read()) 
+                {
+                    time = reader["time"].ToString();
+                    filmID = (int)reader["filmID"];
+                }
+                connenction.Close();
+                connenction.Open();
+                cmd = new SqlCommand("SELECT nimetus FROM filmid WHERE Id = " + filmID, connenction);
+                object result = cmd.ExecuteScalar();
+                connenction.Close();
             }
         }
     }
